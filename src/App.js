f39390d722567371914453f5b6b862dfa1e110e3
Israel Bernabe <israel.bernabe@gmail.com>
2023-08-14 14:17:02 -0400

    Adding socket.io

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import Echo from 'laravel-echo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const cookieValue = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("laravel_session="))
+        ?.split("=")[1];
+
+    async function showCookieValue() {
+        console.log("cookieValue", cookieValue)
+        await getUser()
+    }
+
+    async function getUser() {
+        try {
+            const response = await axios.get(
+                'http://localhost/api/v2/live-class',
+                {
+                    withCredentials: true,
+                },
+            );
+            console.log(response.data)
+            return response.data;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    showCookieValue()
+    window.Echo = new Echo({
+        broadcaster: 'socket.io',
+        host: `${window.location.hostname}:6001`,
+        transports: ['websocket'],
+        authEndpoint: 'locahost/broadcasting/auth',
+    });
+    window.Echo.private('events')
+        .listen('RealTimeMessage', (e) => console.log('RealTimeMessage: ' + e.message));
+    window.Echo.private('App.Models.User.7310')
+    .notification((notification) => {
+        console.log(notification);
+        console.log(notification.message);
+    });
+    return (
+        <div className="App">
+            <h1>Iframe</h1>
+        </div>
+    );
 }
 
 export default App;
